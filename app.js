53e10f90a49431b5637ab47df15ff3d50d708078
SamDeeven <samdeevn@gmail.com>
2022-01-24 13:34:17 +0530

    commit5

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const { MONGOURI } = require("./keys");
-const cors = require("cors");
 
-app.use(cors());
 
 const PORT = process.env.PORT || 5001
 
